281f0fad1d93aafa7360ac8d3ff6b1c3be94e196
Fernando Dias <fernando.dias@nexcore.com.br>
2024-08-06 08:53:44 -0300

    Atualizando o projeto parar melhorar compatibilidade

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    modules: [
-        '@nuxt/ui'
-    ],
-    tailwindcss:{
-        exposeConfig:{
-            level:2, 
-            write:true
-        },
-        viewer: true,
-        cssPath: ['~/assets/css/tailwind.css', {injectPosition: 'first'}],
-        configPath: 'tailwind.config',
-        config:{
+  devtools: { enabled: true },
 
-        },
-    }   
+  modules: [
+      '@nuxt/ui'
+  ],
+
+  tailwindcss:{
+      exposeConfig:{
+          level:2, 
+          write:true
+      },
+      viewer: true,
+      cssPath: ['./assets/css/tailwind.css', {injectPosition: 'first'}],
+      configPath: 'tailwind.config',
+      config:{
+
+      },
+  },
+
+  compatibilityDate: '2024-08-06'
 })
